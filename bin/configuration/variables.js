8d1fd2b09a130e6c52d247453eab72941ab6281d
Bruno Maia <brulao@gmail.com>
2019-04-16 11:10:49 -0400

    Adicionada autenticaçao do usuario

--- bin/configuration/variables.js
+++ bin/configuration/variables.js
@@ -4,6 +4,9 @@ const variables = {
     },
     Database: {
         connection: process.env.connection || 'mongodb+srv://bertuoladmin:bertuol123456@bertuoldb-aybrl.mongodb.net/bertuol?retryWrites=true'
+    },
+    Security: {
+        secretKey: 'e3928a3bc4be46516aa33a79bbdfdb08|cb6caa35194aee95fffa72f737c4fabb'
     }
 }
 module.exports = variables;
--- controllers/usuario-controller.js
+++ controllers/usuario-controller.js
@@ -5,6 +5,8 @@ const validation = require('../bin/helpers/validation');
 const ctrlBase = require('../bin/base/controller-base');
 const _repo = new repository();
 const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+const variables = require('../bin/configuration/variables');
 
 function usuarioController() {
 
@@ -65,6 +67,31 @@ usuarioController.prototype.getById = async (req, res) => {
 // Remover por id
 usuarioController.prototype.delete = async (req, res) => {
     ctrlBase.delete(_repo, req, res);
+};
+
+usuarioController.prototype.autenticar = async (req, res) => {
+    // Criar um validador
+    let _validationContract = new validation();
+    // Critérios de validação
+    _validationContract.isRequired(req.body.email, 'Informe seu email.');
+    _validationContract.isEmail(req.body.email, 'Email inválido.');
+    _validationContract.isRequired(req.body.senha, 'Informe sua senha.');
+
+    if (!_validationContract.isValid()){
+        res.status(400).send({ message: 'Não foi possível efetuar o login.', validation: _validationContract.errors() });
+        return;
+    }
+
+    let usuarioEncontrado = await _repo.authenticate(req.body.email, req.body.senha);
+    
+    if (usuarioEncontrado) {
+        res.status(200).send({
+            usuario: usuarioEncontrado,
+            token: jwt.sign({ user: usuarioEncontrado }, variables.Security.secretKey)
+        })
+    } else {
+        res.status(404).send({messsage: 'Usuário ou senha são inválidos.'});
+    }
 };
 
 module.exports = usuarioController;
