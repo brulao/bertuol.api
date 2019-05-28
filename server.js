7faebcf62003d11105153fd71288c08474a08307
Bruno Maia <brulao@gmail.com>
2019-05-27 20:52:02 -0400

    First node app deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const app = require('../bertuol.api/bin/express');
-const variables = require('../bertuol.api/bin/configuration/variables');
+const app = require('./bin/express');
+const variables = require('./bin/configuration/variables');
 
 app.listen(variables.Api.port, () => {
     console.info(`Api inicializada com sucesso na porta ${variables.Api.port}`);
