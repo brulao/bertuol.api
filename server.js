c9beea7800769b673eaddd5d2582d47265ff9ee4
Bruno Maia <brulao@gmail.com>
2019-05-27 21:23:22 -0400

    First node app deploy

--- server.js
+++ server.js
@@ -3,6 +3,6 @@
 const app = require('./bin/express');
 const variables = require('./bin/configuration/variables');
 
-app.listen(variables.Api.port, () => {
+app.listen(variables.Api.port, "0.0.0.0", () => {
     console.info(`Api inicializada com sucesso na porta ${variables.Api.port}`);
 });
